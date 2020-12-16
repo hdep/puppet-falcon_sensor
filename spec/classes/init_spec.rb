56c9da570e19b3a689d1b983cd5b53b5f05e6097
hugo.deprez <hugo.deprez@sodexo.com>
2020-12-16 17:18:02 +0100

    Fix rspec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
 describe 'falcon_sensor' do
-
   context 'with defaults for all parameters' do
     it {
-      should contain_class('falcon_sensor')
-      should contain_package('falcon-sensor')
+      is_expected.to contain_class('falcon_sensor')
+      is_expected.to contain_package('falcon-sensor')
     }
   end
 
@@ -12,10 +11,10 @@ describe 'falcon_sensor' do
     let(:params) {{
       :ensure => 'absent'
     }}
-    end
+    
     it {
-      should contain_class('falcon_sensor')
-      should contain_package('falcon-sensor').with_ensure('absent')
+      is_expected.to contain_class('falcon_sensor')
+      is_expected.to contain_package('falcon-sensor').with_ensure('absent')
     }
   end
 
@@ -23,11 +22,10 @@ describe 'falcon_sensor' do
     let(:params) {{
       :autoupgrade => true
     }}
-    end
+    
     it {
-      should contain_class('falcon_sensor')
-      should contain_package('falcon-sensor').with_ensure('latest')
+      is_expected.to contain_class('falcon_sensor')
+      is_expected.to contain_package('falcon-sensor').with_ensure('latest')
     }
   end
-  
 end
